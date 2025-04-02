7c0b00d73433f62a841aa8c1d4f6fd97587bf8dc
Nabi Karampoor <thisisnabi@outlook.com>
2025-04-03 00:29:36 +0330

    Add databse migration

---
+++ Telegraph/Persistence/Migrations/20250402205856_InitialDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Telegraph.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "telegraph");
+
+            migrationBuilder.CreateTable(
+                name: "Letters",
+                schema: "telegraph",
+                columns: table => new
+                {
+                    Slug = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Letters", x => x.Slug);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Letters",
+                schema: "telegraph");
+        }
+    }
+}
